779c683834a86097ad7ac732b666493a2ccab2eb
Clark Tomlinson <fallen013@gmail.com>
2015-05-05 09:40:29 -0400

    adding user as param to recovery check

--- settings/changepassword/controller.php
+++ settings/changepassword/controller.php
@@ -113,7 +113,7 @@ class Controller {
 			$recoveryEnabledForUser = false;
 			if ($recoveryAdminEnabled) {
 				$validRecoveryPassword = $keyManager->checkRecoveryPassword($recoveryPassword);
-				$recoveryEnabledForUser = $recovery->isRecoveryEnabledForUser();
+				$recoveryEnabledForUser = $recovery->isRecoveryEnabledForUser($username);
 			}
 
 			if ($recoveryEnabledForUser && $recoveryPassword === '') {
